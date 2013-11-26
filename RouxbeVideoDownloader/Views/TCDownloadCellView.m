31f7fc3d494ea14cf9e482a2344fddea5d5dbb57
Lee Tze Cheun <leetzecheun@gmail.com>
2013-11-26 14:42:09 +0800

    Improved UI for download cell view.

--- RouxbeVideoDownloader/Views/TCDownloadCellView.m
+++ RouxbeVideoDownloader/Views/TCDownloadCellView.m
@@ -9,7 +9,46 @@
 #import "TCDownloadCellView.h"
 #import "TCDownload.h"
 
+@interface TCDownloadCellView ()
+
+@end
+
 @implementation TCDownloadCellView
+
+#pragma mark - Overridden Methods
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+
+    // Disable highlight of the action button when it's clicked. Otherwise,
+    // the action button's icon will have a higlighted background, which makes
+    // it look ugly.
+    [self.actionButton.cell setHighlightsBy:NSNoCellMask];
+}
+
+- (void)setBackgroundStyle:(NSBackgroundStyle)backgroundStyle
+{
+    [super setBackgroundStyle:backgroundStyle];
+
+    // Progress label text color will not be set automatically by
+    // setBackgroundStyle: because it does not use black or white color.
+    // So, we override this method to set it manually ourselves.
+    switch (backgroundStyle) {
+        case NSBackgroundStyleLight:
+            self.progressLabel.textColor = [self textColorForDownloadState:self.download.state];
+            break;
+
+        case NSBackgroundStyleDark:
+        default:
+            // Always show white text color on dark selected background.
+            self.progressLabel.textColor = [NSColor whiteColor];
+            break;
+    }
+    [[self.progressLabel cell] setBackgroundStyle:backgroundStyle];
+}
+
+#pragma mark - Update View for Download
 
 - (void)setDownload:(TCDownload *)download
 {
@@ -17,55 +56,43 @@
 
     self.titleLabel.stringValue = _download.description;
     self.progressBar.doubleValue = _download.progress.fractionCompleted;
+    self.progressLabel.stringValue = [_download localizedProgressDescription];
 
-    switch (_download.state) {
-        case TCDownloadStateRunning:
-            [self configureViewForRunningDownload:_download];
-            break;
+    self.progressLabel.textColor = [self textColorForDownloadState:_download.state];
+    self.actionButton.image = [self iconForDownloadState:_download.state];
+}
 
-        case TCDownloadStatePaused:
-            [self configureViewForPausedDownload:_download];
-            break;
-
+- (NSColor *)textColorForDownloadState:(TCDownloadState)state
+{
+    switch (state) {
         case TCDownloadStateCompleted:
-            [self configureViewForCompletedDownload:_download];
-            break;
+            // Dark Green Color
+            return [NSColor colorWithSRGBRed:0.0f green:0.5f blue:0.0f alpha:1.0f];
 
         case TCDownloadStateFailed:
-            [self configureViewForFailedDownload:_download];
-            break;
+            return [NSColor redColor];
 
+        case TCDownloadStateRunning:
+        case TCDownloadStatePaused:
         default:
-            break;
+            return [NSColor grayColor];
     }
 }
 
-- (void)configureViewForRunningDownload:(TCDownload *)download
+- (NSImage *)iconForDownloadState:(TCDownloadState)state
 {
-    self.progressLabel.stringValue = [download.progress localizedAdditionalDescription];
-    self.progressLabel.textColor = [NSColor grayColor];
-    self.actionButton.image = [NSImage imageNamed:NSImageNameStopProgressFreestandingTemplate];
-}
+    switch (state) {
+        case TCDownloadStateRunning:
+            return [NSImage imageNamed:NSImageNameStopProgressFreestandingTemplate];
 
-- (void)configureViewForPausedDownload:(TCDownload *)download
-{
-    self.progressLabel.stringValue = NSLocalizedString(@"Paused", @"");
-    self.progressLabel.textColor = [NSColor grayColor];
-    self.actionButton.image = [NSImage imageNamed:NSImageNameRefreshFreestandingTemplate];
-}
+        case TCDownloadStateCompleted:
+            return [NSImage imageNamed:NSImageNameRevealFreestandingTemplate];
 
-- (void)configureViewForCompletedDownload:(TCDownload *)download
-{
-    self.progressLabel.stringValue = NSLocalizedString(@"Completed", @"");
-    self.progressLabel.textColor = [NSColor greenColor];
-    self.actionButton.image = [NSImage imageNamed:NSImageNameRevealFreestandingTemplate];
-}
-
-- (void)configureViewForFailedDownload:(TCDownload *)download
-{
-    self.progressLabel.stringValue = [NSString stringWithFormat:@"Error: %@", download.error];
-    self.progressLabel.textColor = [NSColor redColor];
-    self.actionButton.image = [NSImage imageNamed:NSImageNameRefreshFreestandingTemplate];
+        case TCDownloadStatePaused:
+        case TCDownloadStateFailed:
+        default:
+            return [NSImage imageNamed:NSImageNameRefreshFreestandingTemplate];;
+    }
 }
 
 @end
