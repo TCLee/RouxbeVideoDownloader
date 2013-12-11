b643ffb6651c20db53f6410470dfdf0b0e7e8c8f
Lee Tze Cheun <leetzecheun@gmail.com>
2013-12-11 19:40:37 +0800

    Added window controller to provide the preferences UI.

---
+++ RouxbeVideoDownloader/Controllers/TCPreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  TCPreferencesWindowController.h
+//  RouxbeVideoDownloader
+//
+//  Created by Lee Tze Cheun on 12/11/13.
+//  Copyright (c) 2013 Lee Tze Cheun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TCPreferencesWindowController : NSWindowController
+
+@end
---
+++ RouxbeVideoDownloader/Controllers/TCPreferencesWindowController.m
@@ -0,0 +1,33 @@
+//
+//  TCPreferencesWindowController.m
+//  RouxbeVideoDownloader
+//
+//  Created by Lee Tze Cheun on 12/11/13.
+//  Copyright (c) 2013 Lee Tze Cheun. All rights reserved.
+//
+
+#import "TCPreferencesWindowController.h"
+
+@interface TCPreferencesWindowController ()
+
+@end
+
+@implementation TCPreferencesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
