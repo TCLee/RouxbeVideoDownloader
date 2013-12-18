8d2832d1e1276d6aeae1db83513a6462b01facd1
Lee Tze Cheun <leetzecheun@gmail.com>
2013-12-18 11:00:53 +0800

    Refactored and simplified TCHTTPRequestStub code.

--- RouxbeVideoDownloaderTests/Lib/TCHTTPRequestStub.m
+++ RouxbeVideoDownloaderTests/Lib/TCHTTPRequestStub.m
@@ -23,49 +23,76 @@
         return [request.URL.scheme isEqualToString:@"http"] &&
                [request.URL.host isEqualToString:@"rouxbe.com"];
     } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-        NSArray *pathComponents = request.URL.pathComponents;
-
-        if ([pathComponents[1] isEqualToString:@"cooking-school"]) {
-            // URL = http://rouxbe.com/cooking-school/lessons/170.xml
-            // Path Components = ["/", "cooking-school", "lessons", "170.xml"]
-            return [self responseWithFile:@"Lesson.xml"];
-        } else if ([pathComponents[1] isEqualToString:@"recipes"]) {
-            // URL = http://rouxbe.com/recipes/89.xml
-            // Path Components = ["/", "recipes", "89.xml"]
-            return [self responseWithFile:@"Recipe.xml"];
-        } else if ([pathComponents[1] isEqualToString:@"embedded_player"]) {
-            if ([pathComponents[2] isEqualToString:@"settings_section"]) {
-                // URL = http://rouxbe.com/embedded_player/settings_section/101.xml
-                // Path Components = ["/", "embedded_player", "settings_section", "101.xml"]
-                return [self responseWithFile:@"LessonStepVideo.xml"];
-            } else if ([pathComponents[2] isEqualToString:@"settings_drilldown"]) {
-                // URL = http://rouxbe.com/embedded_player/settings_drilldown/202.xml
-                // Path Components = ["/", "embedded_player", "settings_drilldown", "202.xml"]
-                return [self responseWithFile:@"Tip.xml"];
-            }
-        }
-
-        return [OHHTTPStubsResponse responseWithError:[NSError errorWithDomain:NSURLErrorDomain
-                                                                          code:NSURLErrorUnsupportedURL
-                                                                      userInfo:@{NSLocalizedDescriptionKey: NSLocalizedString(@"Request URL was not stubbed because it does not match any known URL.", nil)}]];
+        return [self responseForRequest:request];
     }];
-    stub.name = @"Rouxbe Web Service Stub";
+    stub.name = @"TCHTTPRequestStub";
 
     return stub;
-}
-
-+ (OHHTTPStubsResponse *)responseWithFile:(NSString *)fileName
-{
-    NSBundle *testBundle = [NSBundle bundleForClass:[self class]];
-
-    return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFileInBundle(fileName, testBundle)
-                                            statusCode:200
-                                               headers:@{@"Content-Type":@"application/xml"}];
 }
 
 + (void)stopStubRequests
 {
     [OHHTTPStubs removeAllStubs];
+}
+
+#pragma mark - Private Helper Methods
+
++ (OHHTTPStubsResponse *)responseForRequest:(NSURLRequest *)request
+{
+    NSArray *pathComponents = request.URL.pathComponents;
+
+    if ([self isLessonPath:pathComponents]) {
+        return [self responseWithFile:@"Lesson.xml"];
+    } else if ([self isRecipePath:pathComponents]) {
+        return [self responseWithFile:@"Recipe.xml"];
+    } else if ([self isLessonStepVideoPath:pathComponents]) {
+        return [self responseWithFile:@"LessonStepVideo.xml"];
+    } else if ([self isTipVideoPath:pathComponents]) {
+        return [self responseWithFile:@"Tip.xml"];
+    }
+
+    return [OHHTTPStubsResponse responseWithError:[NSError errorWithDomain:NSURLErrorDomain
+                                                                      code:NSURLErrorUnsupportedURL
+                                                                  userInfo:@{NSLocalizedDescriptionKey: NSLocalizedString(@"Request URL was not stubbed because it does not match any known URL.", nil)}]];
+}
+
++ (OHHTTPStubsResponse *)responseWithFile:(NSString *)fileName
+{
+    NSBundle *testBundle = [NSBundle bundleForClass:[self class]];
+    
+    return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFileInBundle(fileName, testBundle)
+                                            statusCode:200
+                                               headers:@{@"Content-Type":@"application/xml"}];
+}
+
++ (BOOL)isLessonPath:(NSArray *)pathComponents
+{
+    // Example URL = http://rouxbe.com/cooking-school/lessons/170.xml
+    // Path Components = ["/", "cooking-school", "lessons", "170.xml"]
+    return [pathComponents[1] isEqualToString:@"cooking-school"];
+}
+
++ (BOOL)isLessonStepVideoPath:(NSArray *)pathComponents
+{
+    // Example URL = http://rouxbe.com/embedded_player/settings_section/101.xml
+    // Path Components = ["/", "embedded_player", "settings_section", "101.xml"]
+    return [pathComponents[1] isEqualToString:@"embedded_player"] &&
+           [pathComponents[2] isEqualToString:@"settings_section"];
+}
+
++ (BOOL)isRecipePath:(NSArray *)pathComponents
+{
+    // Example URL = http://rouxbe.com/recipes/89.xml
+    // Path Components = ["/", "recipes", "89.xml"]
+    return [pathComponents[1] isEqualToString:@"recipes"];
+}
+
++ (BOOL)isTipVideoPath:(NSArray *)pathComponents
+{
+    // Example URL = http://rouxbe.com/embedded_player/settings_drilldown/202.xml
+    // Path Components = ["/", "embedded_player", "settings_drilldown", "202.xml"]
+    return [pathComponents[1] isEqualToString:@"embedded_player"] &&
+           [pathComponents[2] isEqualToString:@"settings_drilldown"];
 }
 
 @end
