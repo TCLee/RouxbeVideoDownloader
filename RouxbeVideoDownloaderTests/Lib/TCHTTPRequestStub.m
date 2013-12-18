ae8e899d609a1c0e46de2a65dff19fe5e0c70113
Lee Tze Cheun <leetzecheun@gmail.com>
2013-12-18 10:37:49 +0800

    Added comments for path routing.

--- RouxbeVideoDownloaderTests/Lib/TCHTTPRequestStub.m
+++ RouxbeVideoDownloaderTests/Lib/TCHTTPRequestStub.m
@@ -20,18 +20,27 @@
 + (id<OHHTTPStubsDescriptor>)beginStubRequests
 {
     id<OHHTTPStubsDescriptor> stub = [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        return [request.URL.host isEqualToString:@"rouxbe.com"];
+        return [request.URL.scheme isEqualToString:@"http"] &&
+               [request.URL.host isEqualToString:@"rouxbe.com"];
     } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
         NSArray *pathComponents = request.URL.pathComponents;
 
         if ([pathComponents[1] isEqualToString:@"cooking-school"]) {
+            // URL = http://rouxbe.com/cooking-school/lessons/170.xml
+            // Path Components = ["/", "cooking-school", "lessons", "170.xml"]
             return [self responseWithFile:@"Lesson.xml"];
         } else if ([pathComponents[1] isEqualToString:@"recipes"]) {
+            // URL = http://rouxbe.com/recipes/89.xml
+            // Path Components = ["/", "recipes", "89.xml"]
             return [self responseWithFile:@"Recipe.xml"];
         } else if ([pathComponents[1] isEqualToString:@"embedded_player"]) {
             if ([pathComponents[2] isEqualToString:@"settings_section"]) {
+                // URL = http://rouxbe.com/embedded_player/settings_section/101.xml
+                // Path Components = ["/", "embedded_player", "settings_section", "101.xml"]
                 return [self responseWithFile:@"LessonStepVideo.xml"];
             } else if ([pathComponents[2] isEqualToString:@"settings_drilldown"]) {
+                // URL = http://rouxbe.com/embedded_player/settings_drilldown/202.xml
+                // Path Components = ["/", "embedded_player", "settings_drilldown", "202.xml"]
                 return [self responseWithFile:@"Tip.xml"];
             }
         }
