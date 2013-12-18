ad0193f434f464d89d209a3de770ab622f42448b
Lee Tze Cheun <leetzecheun@gmail.com>
2013-12-18 20:34:18 +0800

    Converted TCLessonStepTests to use OHHTTPStubs instead of OCMock.

--- RouxbeVideoDownloaderTests/TCLessonStepTests.m
+++ RouxbeVideoDownloaderTests/TCLessonStepTests.m
@@ -9,15 +9,13 @@
 @import XCTest;
 
 #import "TCTestDataLoader.h"
+#import "TCGroup.h"
 #import "TCStep+Lesson.h"
 
 @interface TCLessonStepTests : XCTestCase
 
-@property (readwrite, nonatomic, strong) AFHTTPRequestOperation *actualOperation;
-@property (readwrite, nonatomic, strong) id mockOperation;
-
-@property (readwrite, nonatomic, strong) NSURL *blockVideoURL;
-@property (readwrite, nonatomic, strong) NSError *blockError;
+@property (readwrite, nonatomic, strong) TCGroup *group;
+@property (readwrite, nonatomic, strong) TCStep *step;
 
 @end
 
@@ -26,60 +24,67 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 
-    TCStep *step = [[TCStep alloc] init];
-
-    // Save the video URL and error values from the completion block.
-    self.actualOperation = [step videoURLRequestOperationWithCompleteBlock:^(NSURL *videoURL, NSError *error) {
-        self.blockVideoURL = videoURL;
-        self.blockError = error;
-    }];
-
-    // Create a mock of AFHTTPRequestOperation, so that we don't actually hit
-    // the network when the start method is called.
-    self.mockOperation = [OCMockObject partialMockForObject:self.actualOperation];
-    [[[self.mockOperation stub] andDo:^(NSInvocation *invocation) {
-        [self.mockOperation completionBlock]();
-    }] start];
+    self.group = [[TCGroup alloc] initWithXMLData:[TCTestDataLoader XMLDataWithName:@"Lesson"]
+                                     stepsXMLPath:@"recipesteps.recipestep"];
+    
+    // Pick a step without a video URL.
+    self.step = self.group.steps[1];
 }
 
 - (void)tearDown
 {
-    [self.mockOperation stopMocking];
-    self.mockOperation = nil;
-
-    self.actualOperation = nil;
-    self.blockVideoURL = nil;
-    self.blockError = nil;
-
     [super tearDown];
+
+    [OHHTTPStubs removeAllStubs];
+    self.group = nil;
+    self.step = nil;
 }
 
-- (void)testCanParseVideoURLFromXMLData
+- (void)testCanParseVideoURLFromXML
 {
-    [[[self.mockOperation stub]
-      andReturn:[TCTestDataLoader XMLDataWithName:@"LessonStepVideo"]]
-     responseObject];
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"rouxbe.com"];
+    } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFileInBundle(@"LessonStepVideo.xml", nil)
+                                                statusCode:200
+                                                   headers:@{@"Content-Type":@"application/xml"}];
+    }];
 
-    [self.actualOperation start];
+    __block NSURL *blockVideoURL = nil;
+    __block NSError *blockError = nil;
+    AFHTTPRequestOperation *requestOperation = [self.step videoURLRequestOperationWithCompleteBlock:^(NSURL *videoURL, NSError *error) {
+        blockVideoURL = videoURL;
+        blockError = error;
+    }];
+    [requestOperation start];
 
-    expect(self.blockVideoURL).will.equal([NSURL URLWithString:@"http://media.rouxbe.com/h264/CS_Knives_L3_T02.f4v"]);
-    expect(self.blockError).will.beNil();
+    expect(blockVideoURL).willNot.beNil();
+    expect(blockError).will.beNil();
+    expect(self.step.videoURL).will.equal(blockVideoURL);
 }
 
 - (void)testErrorIsSetOnFailure
 {
-    NSError *dummyError = [NSError errorWithDomain:NSURLErrorDomain
-                                              code:NSURLErrorTimedOut
-                                          userInfo:nil];
-    [[[self.mockOperation stub] andReturn:dummyError] error];
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"rouxbe.com"];
+    } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithError: [NSError errorWithDomain:NSURLErrorDomain
+                                                                           code:NSURLErrorNotConnectedToInternet
+                                                                       userInfo:nil]];
+    }];
 
-    [self.actualOperation start];
+    __block NSURL *blockVideoURL = nil;
+    __block NSError *blockError = nil;
+    AFHTTPRequestOperation *requestOperation = [self.step videoURLRequestOperationWithCompleteBlock:^(NSURL *videoURL, NSError *error) {
+        blockVideoURL = videoURL;
+        blockError = error;
+    }];
+    [requestOperation start];
 
-    expect(self.blockVideoURL).will.beNil();
-    expect(self.blockError).willNot.beNil();
-    expect(self.blockError.code).will.equal(NSURLErrorTimedOut);
+    expect(blockVideoURL).will.beNil();
+    expect(blockError).willNot.beNil();
+    expect(blockError.code).will.equal(NSURLErrorNotConnectedToInternet);
 }
 
 @end
