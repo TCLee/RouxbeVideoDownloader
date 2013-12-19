9ff27e3d2db21ec701663133f38e3b0077bc1422
Lee Tze Cheun <leetzecheun@gmail.com>
2013-12-19 16:16:16 +0800

    Refactored TCVideo class and added more tests for it.

--- RouxbeVideoDownloader/Models/Video/TCVideo.m
+++ RouxbeVideoDownloader/Models/Video/TCVideo.m
@@ -21,23 +21,7 @@
  * URL to get the MP4 version of the video rather than the default Flash video.
  * Downloading the video in MP4 makes it compatible with iOS devices.
  */
-static NSString * const kMP4VideoURLString = @"http://media.rouxbe.com/itouch/mp4/%@.mp4";
-
-/**
- * Returns a URL to the MP4 version of the video from the given Flash video URL.
- *
- * @param flashVideoURL The URL to the Flash version of the video.
- */
-FOUNDATION_STATIC_INLINE NSURL *MP4VideoURLFromFlashVideoURL(NSURL *flashVideoURL)
-{
-    if (!flashVideoURL) { return nil; }
-
-    // Delete the ".f4v" (Flash) file extension. We just want the file name only.
-    NSString *videoName = [[flashVideoURL lastPathComponent] stringByDeletingPathExtension];
-
-    // Create the URL from the template string and return the URL to the MP4 video.
-    return [NSURL URLWithString:[NSString stringWithFormat:kMP4VideoURLString, videoName]];
-}
+static NSString * const TCMP4VideoURLString = @"http://media.rouxbe.com/itouch/mp4/%@.mp4";
 
 @implementation TCVideo
 
@@ -55,7 +39,7 @@ FOUNDATION_STATIC_INLINE NSURL *MP4VideoURLFromFlashVideoURL(NSURL *flashVideoUR
 
     self = [super init];
     if (self) {
-        _sourceURL = MP4VideoURLFromFlashVideoURL(sourceURL);
+        _sourceURL = [[self class] MP4VideoURLFromFlashVideoURL:sourceURL];
         _group = [group copy];
         _title = [title copy];
         _position = position;
@@ -69,6 +53,22 @@ FOUNDATION_STATIC_INLINE NSURL *MP4VideoURLFromFlashVideoURL(NSURL *flashVideoUR
                              group:nil
                              title:title
                           position:NSNotFound];
+}
+
+/**
+ * Returns a URL to the MP4 version of the video from the given Flash video URL.
+ *
+ * @param flashVideoURL The URL to the Flash version of the video.
+ */
++ (NSURL *)MP4VideoURLFromFlashVideoURL:(NSURL *)flashVideoURL
+{
+    if (!flashVideoURL) { return nil; }
+
+    // Delete the ".f4v" (Flash) file extension. We just want the file name only.
+    NSString *videoName = [[flashVideoURL lastPathComponent] stringByDeletingPathExtension];
+
+    // Create the URL from the template string and return the URL to the MP4 video.
+    return [NSURL URLWithString:[NSString stringWithFormat:TCMP4VideoURLString, videoName]];
 }
 
 #pragma mark - Destination Path Component
@@ -93,34 +93,35 @@ FOUNDATION_STATIC_INLINE NSURL *MP4VideoURLFromFlashVideoURL(NSURL *flashVideoUR
 
 #pragma mark - Find Videos from URL
 
-+ (AFHTTPRequestOperation *)getVideosFromURL:(NSURL *)aURL
++ (AFHTTPRequestOperation *)getVideosFromURL:(NSURL *)theURL
                                completeBlock:(TCVideoCompleteBlock)completeBlock
 {
-    NSParameterAssert(aURL);
+    NSParameterAssert(theURL);
 
-    // Determine the resource category from the URL.
-    switch ([aURL rouxbeCategory]) {
+    switch ([theURL rouxbeCategory]) {
         case TCRouxbeCategoryLesson:
-            return [TCLesson getLessonWithID:[aURL rouxbeID]
-                               completeBlock:[self groupCompleteBlockWithVideoCompleteBlock:completeBlock]];
+            return [TCLesson getLessonWithID:[theURL rouxbeID] completeBlock:^(TCGroup *group, NSError *error) {
+                if (completeBlock) {
+                    completeBlock([self videosFromGroup:group], error);
+                }
+            }];
 
         case TCRouxbeCategoryRecipe:
-            return [TCRecipe getRecipeWithID:[aURL rouxbeID]
-                               completeBlock:[self groupCompleteBlockWithVideoCompleteBlock:completeBlock]];
+            return [TCRecipe getRecipeWithID:[theURL rouxbeID] completeBlock:^(TCGroup *group, NSError *error) {
+                if (completeBlock) {
+                    completeBlock([self videosFromGroup:group], error);
+                }
+            }];
 
         case TCRouxbeCategoryTip:
-            return [TCTip getTipWithID:[aURL rouxbeID] completeBlock:^(TCTip *tip, NSError *error) {
-                TCVideo *video = tip ? [[TCVideo alloc] initWithSourceURL:tip.videoURL title:tip.name] : nil;
-                NSArray *videos = video ? @[video] : nil;
-
+            return [TCTip getTipWithID:[theURL rouxbeID] completeBlock:^(TCTip *tip, NSError *error) {
                 if (completeBlock) {
-                    completeBlock(videos, error);
+                    completeBlock([self videosFromTip:tip], error);
                 }
             }];
 
         case TCRouxbeCategoryUnknown:
         default: {
-            // Error - Invalid Rouxbe URL
             NSError *error = [[NSError alloc] initWithDomain:NSURLErrorDomain
                                                         code:NSURLErrorUnsupportedURL
                                                     userInfo:@{NSLocalizedDescriptionKey: @"The URL is not a valid rouxbe.com URL.",
@@ -133,25 +134,35 @@ FOUNDATION_STATIC_INLINE NSURL *MP4VideoURLFromFlashVideoURL(NSURL *flashVideoUR
     }
 }
 
-+ (TCGroupCompleteBlock)groupCompleteBlockWithVideoCompleteBlock:(TCVideoCompleteBlock)completeBlock
+/**
+ * Returns an array of video objects from the given group object.
+ * Each video will represent one step from the group.
+ */
++ (NSArray *)videosFromGroup:(TCGroup *)group
 {
-    return ^(TCGroup *group, NSError *error) {
-        NSMutableArray *mutableVideos = nil;
-        if (group) {
-            mutableVideos = [[NSMutableArray alloc] initWithCapacity:group.steps.count];
-            for (TCStep *step in group.steps) {
-                TCVideo *video = [[TCVideo alloc] initWithSourceURL:step.videoURL
-                                                              group:step.groupName
-                                                              title:step.name
-                                                           position:step.position];
-                [mutableVideos addObject:video];
-            }
-        }
+    if (!group) { return nil; }
 
-        if (completeBlock) {
-            completeBlock(mutableVideos, error);
-        }
-    };
+    NSMutableArray *mutableVideos = [[NSMutableArray alloc] initWithCapacity:group.steps.count];
+    for (TCStep *step in group.steps) {
+        TCVideo *video = [[TCVideo alloc] initWithSourceURL:step.videoURL
+                                                      group:step.groupName
+                                                      title:step.name
+                                                   position:step.position];
+        [mutableVideos addObject:video];
+    }
+    return mutableVideos;
+}
+
+/**
+ * Returns an array with one video object from the given tip object.
+ */
++ (NSArray *)videosFromTip:(TCTip *)tip
+{
+    if (!tip) { return nil; }
+
+    TCVideo *video = [[TCVideo alloc] initWithSourceURL:tip.videoURL
+                                                  title:tip.name];
+    return @[video];
 }
 
 @end
--- RouxbeVideoDownloaderTests/TCVideoTests.m
+++ RouxbeVideoDownloaderTests/TCVideoTests.m
@@ -9,6 +9,7 @@
 @import XCTest;
 
 #import "TCVideo.h"
+#import "TCHTTPRequestStub.h"
 
 @interface TCVideoTests : XCTestCase
 
@@ -24,6 +25,8 @@
 {
     [super setUp];
 
+    [TCHTTPRequestStub beginStubRequests];
+
     self.dummySourceURL = [NSURL URLWithString:@"http://media.rouxbe.com/h264/Cs_Eggs_L2_T2.f4v"];
     self.dummyGroup = @"The Video's Group";
     self.dummyTitle = @"Title of Video";
@@ -31,11 +34,13 @@
 
 - (void)tearDown
 {
+    [super tearDown];
+
+    [TCHTTPRequestStub stopStubRequests];
+
     self.dummySourceURL = nil;
     self.dummyGroup = nil;
     self.dummyTitle = nil;
-
-    [super tearDown];
 }
 
 #pragma mark - Init Method Tests
@@ -104,20 +109,125 @@
 
 #pragma mark - Get Videos From URL Tests
 
-- (void)testShouldReturnErrorIfURLIsInvalid
+- (void)testGivenALessonURLShouldReturnLessonVideos
 {
-    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://www.google.com"] completeBlock:^(NSArray *videos, NSError *error) {
-        expect(videos).to.beNil();
-        expect(error).toNot.beNil();
+    __block TCVideo *blockVideo = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/cooking-school/lessons/241"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideo = videos[1];
     }];
+
+    expect(blockVideo.group).will.equal(@"Eggs | Frying, Basting & Poaching");
+    expect(blockVideo.title).will.equal(@"How to Pan Fry Eggs");
+    expect(blockVideo.sourceURL).will.equal([NSURL URLWithString:@"http://media.rouxbe.com/itouch/mp4/Cs_Eggs_L2_T2.mp4"]);
+    expect(blockVideo.position).will.equal(1);
 }
 
-- (void)testShouldRaiseExceptionWhenSearchURLIsNil
+- (void)testGivenARecipeURLShouldReturnRecipeVideos
+{
+    __block TCVideo *blockVideo = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/recipes/89"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideo = videos[0];
+    }];
+
+    expect(blockVideo.group).will.equal(@"Chicken Cashew");
+    expect(blockVideo.title).will.equal(@"Making the Sauce");
+    expect(blockVideo.sourceURL).will.equal([NSURL URLWithString:@"http://media.rouxbe.com/itouch/mp4/Chicken_Cashew_S1.mp4"]);
+    expect(blockVideo.position).will.equal(0);
+}
+
+- (void)testGivenATipURLShouldReturnATipVideo
+{
+    __block NSArray *blockVideos = nil;
+    __block TCVideo *blockVideo = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/tips-techniques/117"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideos = videos;
+        blockVideo = videos[0];
+    }];
+
+    expect(blockVideo.group).will.beNil();
+    expect(blockVideo.title).will.equal(@"What is Thai Basil?");
+    expect(blockVideo.sourceURL).will.equal([NSURL URLWithString:@"http://media.rouxbe.com/itouch/mp4/DD_ThaiBasil.mp4"]);
+    expect(blockVideo.position).will.equal(NSNotFound);
+
+    expect(blockVideos).will.haveCountOf(1);
+}
+
+- (void)testFailToFetchLessonVideosShouldCallCompletionBlockWithError
+{
+    [TCHTTPRequestStub stubLessonRequestToReturnResponseWithError:
+     [NSError errorWithDomain:NSURLErrorDomain
+                         code:NSURLErrorTimedOut
+                     userInfo:nil]];
+
+    __block NSArray *blockVideos = nil;
+    __block NSError *blockError = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/cooking-school/lessons/241"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideos = videos;
+        blockError = error;
+    }];
+
+    expect(blockVideos).will.beNil();
+    expect(blockError).willNot.beNil();
+    expect(blockError.code).will.equal(NSURLErrorTimedOut);
+}
+
+- (void)testFailToFetchRecipeVideosShouldCallCompletionBlockWithError
+{
+    [TCHTTPRequestStub stubRecipeRequestToReturnResponseWithError:
+     [NSError errorWithDomain:NSURLErrorDomain
+                         code:NSURLErrorNotConnectedToInternet
+                     userInfo:nil]];
+
+    __block NSArray *blockVideos = nil;
+    __block NSError *blockError = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/recipes/89"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideos = videos;
+        blockError = error;
+    }];
+
+    expect(blockVideos).will.beNil();
+    expect(blockError).willNot.beNil();
+    expect(blockError.code).will.equal(NSURLErrorNotConnectedToInternet);
+}
+
+- (void)testFailToFetchTipVideoShouldCallCompletionBlockWithError
+{
+    [TCHTTPRequestStub stubTipRequestToReturnResponseWithError:
+     [NSError errorWithDomain:NSURLErrorDomain
+                         code:NSURLErrorResourceUnavailable
+                     userInfo:nil]];
+
+    __block NSArray *blockVideos = nil;
+    __block NSError *blockError = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://rouxbe.com/tips-techniques/117"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideos = videos;
+        blockError = error;
+    }];
+
+    expect(blockVideos).will.beNil();
+    expect(blockError).willNot.beNil();
+    expect(blockError.code).will.equal(NSURLErrorResourceUnavailable);
+}
+
+- (void)testGivenAnInvalidURLShouldReturnError
+{
+    __block NSArray *blockVideos = nil;
+    __block NSError *blockError = nil;
+    [TCVideo getVideosFromURL:[NSURL URLWithString:@"http://www.google.com"] completeBlock:^(NSArray *videos, NSError *error) {
+        blockVideos = videos;
+        blockError = error;
+    }];
+
+    expect(blockVideos).will.beNil();
+    expect(blockError).willNot.beNil();
+    expect(blockError.code).will.equal(NSURLErrorUnsupportedURL);
+}
+
+- (void)testGivenNilURLShouldRaiseException
 {
     void(^blockToTest)() = ^() {
         [TCVideo getVideosFromURL:nil completeBlock:nil];
     };
-
     expect(blockToTest).to.raise(NSInternalInconsistencyException);
 }
 
